3d70668e3b3846982cac7f433e46bcbf7a065f4b
Sumit <sumit13thakur124@gmail.com>
2025-05-24 19:04:29 +0530

    Successfully Implemented Video.Controllers.js

--- src/controllers/Video.controllers.js
+++ src/controllers/Video.controllers.js
@@ -4,21 +4,27 @@ import { ErrorHandler } from "../utils/ApiErrorHandler.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 import { Video } from "../models/video.model.js";
 import { uploadResult, deleteFromCloudinary } from "../utils/cloudinary.js";
+import fs from "fs";
+import mongoose from "mongoose";
 
 
 // get all videos On the Based on Title 
-const Get_All_Videos = asyncHandler(async (req, res, next) => {
+const Get_All_Videos = asyncHandler(async (req, res) => {
     const {Title } = req.body
     const videos = await Video.find({Title});
     return res.status(200).json(new ApiResponse(200, "success", {videos}));
 })
 
+
+
 // upload video and Upload video in cloudinary 
-const Upload_Video = asyncHandler(async (req, res, next) => {
+const Upload_Video = asyncHandler(async (req, res) => {
     let { Title, Description } = req.body;
     const UserDetails = req.User;
-
+    
     if (!Title) {
+        if(localVideoPath) fs.unlinkSync(localVideoPath);
+        if(localThumbnailPath) fs.unlinkSync(localThumbnailPath);
         throw new ErrorHandler(400, "Title Required");
     }
 
@@ -30,14 +36,17 @@ const Upload_Video = asyncHandler(async (req, res, next) => {
     const localThumbnailPath = req.files?.Thumbnail?.[0]?.path;
 
     if (!localVideoPath) {
+        if(localThumbnailPath) fs.unlinkSync(localThumbnailPath);
         throw new ErrorHandler(400, "Video File Required");
     }
     if (!localThumbnailPath) {
+        if(localVideoPath) fs.unlinkSync(localVideoPath);
         throw new ErrorHandler(400, "Thumbnail File Required");
     }
 
     let uploadedVideo = null;
     let uploadedThumbnail = null;
+
     try {
         // Upload to cloud
         uploadedVideo = await uploadResult(localVideoPath, "video");
@@ -63,6 +72,9 @@ const Upload_Video = asyncHandler(async (req, res, next) => {
         console.error("Upload error:", error);
         if(uploadedVideo) await deleteFromCloudinary(uploadedVideo.url,"video");
         if(uploadedThumbnail) await deleteFromCloudinary(uploadedThumbnail.url,"thumbnail");
+        console.log("File deleted from local path:",localVideoPath);
+        if(localVideoPath) fs.unlinkSync(localVideoPath);
+        if(localThumbnailPath) fs.unlinkSync(localThumbnailPath);
         throw new ErrorHandler(500, "Failed to upload video or thumbnail");
     }
 });
@@ -70,20 +82,22 @@ const Upload_Video = asyncHandler(async (req, res, next) => {
 
 
 // searching By Video Id
-const Search_Video_By_Id = asyncHandler(async (req, res, next) => {
-    const {VideoId } = req.params
-    const videos = await Video.find({_id:VideoId});
+const Search_Video_By_Id = asyncHandler(async (req, res) => {
+    const VideoId = req.params.VideoId
+    console.log(VideoId);
+    
+    const videos = await Video.findById(VideoId);
     return res.status(200).json(new ApiResponse(200, "success", {videos}));
 })
 
 //Update Video 
-const Update_Video = asyncHandler(async (req, res, next) => {
+const Update_Video = asyncHandler(async (req, res) => {
     const {VideoId } = req.params
     const {Title} = req.body
     const {Description} = req.body
     const video = await Video.findById(VideoId);
-    if(!Video){
-        throw new ErrorHandler(404,"Video Not Found");
+    if(!video){
+        throw new ErrorHandler(404,"Video_Owner Not Found");
     }
     if(!video.Owner === req.User._id){
         throw new ErrorHandler(401,"You Cant Update This VideoDetails");
@@ -110,19 +124,24 @@ const Update_Video = asyncHandler(async (req, res, next) => {
 })
 
 //Delete Video 
-const Delete_Video = asyncHandler(async (req, res, next) => {
+const Delete_Video = asyncHandler(async (req, res) => {
     try {
         const {VideoId } = req.params
-        const video = await Video.findById(VideoId);
+        const video = await Video.findByIdAndDelete(VideoId);
         if(!video){
             throw new ErrorHandler(404,"Video Not Found");
         }
         if(! video.Owner === req.User._id){
             throw new ErrorHandler(401,"You Cant Delete This VideoDetails");
         }
-        await deleteFromCloudinary(video.videoFile,"video");
-        await video.remove();
+
+        await deleteFromCloudinary(video.VideoFile,"video");
+        await deleteFromCloudinary(video.Thumbnail,"thumbnail");
+        const deletedVideo = await Video.findById(VideoId);
+
+        console.log("Video after deletion:", deletedVideo);
         return res.status(200).json(new ApiResponse(200, "success", {video}));
+
     } catch (error) {
         throw new ErrorHandler(500,"Error While Deleting Video ");
         
--- src/routes/Video.route.js
+++ src/routes/Video.route.js
@@ -3,13 +3,17 @@ import { Router } from "express";
 import {
     Get_All_Videos,
     Upload_Video,
+    Search_Video_By_Id,
+    Update_Video,
+    Delete_Video
 } from "../controllers/Video.controllers.js";
 import upload from "../middlewares/Multer.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/").get(Get_All_Videos);
+router.route("/Search/:VideoId").get(Search_Video_By_Id);
+router.route("/get_All_Videos").get(Get_All_Videos);
 router.route("/Upload").post(
     verifyJwt,
     upload.fields([
@@ -18,5 +22,23 @@ router.route("/Upload").post(
     ]),
     Upload_Video
   );
+
+  router.route("/UpdateVideo/:VideoId").put(
+    verifyJwt,
+    upload.fields([
+      { name: "Video", maxCount: 1 },
+    ]),
+    Update_Video
+  );
+
+  router.route("/DeleteVideo/:VideoId").delete(
+    verifyJwt,
+    upload.fields([
+      { name: "Video", maxCount: 1 },
+    ]),
+    Delete_Video
+  );
+
+
 
 export default router;
